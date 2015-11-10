457f8abfa24feecb38c8086d9c154e002a7c1ab7
bzsmz1990 <bzsmz1990@gmail.com>
2015-11-10 15:02:49 -0500

    finish lesson2 project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+from sklearn import svm
+clf = svm.SVC(kernel = 'rbf', C = 10000)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+# from sklearn.metrics import accuracy_score
+# print accuracy_score(pred, labels_test)
+sum = 0
+for i in range(0, len(pred)):
+    if pred[i] == 1:
+        sum += 1
+
+print 'Chris: ', sum
 #########################################################
 
 
