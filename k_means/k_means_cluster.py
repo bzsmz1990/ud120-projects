ae4128ca93f3d9d0299b351dbd816677a3a528ec
bzsmz1990 <bzsmz1990@gmail.com>
2015-11-22 01:09:41 -0500

    finish lesson8 mini-project

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,10 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
+# feature_3 = "total_payments"
+poi = "poi"
 features_list = [poi, feature_1, feature_2]
+# features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,6 +60,10 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
+# for f1, f2, _ in finance_features:
+#     plt.scatter( f1, f2 )
+# plt.show()
+
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
@@ -65,6 +71,41 @@ plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+# do scale to [0.0, 1.0]
+
+
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred = kmeans.predict(finance_features)
+
+minESO = sys.maxint
+maxESO = -1
+minSal = sys.maxint
+maxSal = -1
+for key, value in data_dict.iteritems():
+    val1 = value["exercised_stock_options"]
+    if val1 == "NaN":
+        continue
+    if minESO > val1:
+        minESO = val1
+    if maxESO < val1:
+        maxESO = val1
+
+    val2 = value["salary"]
+    if val2 == "NaN":
+        continue
+    if minSal > val2:
+        minSal = val2
+    if maxSal < val2:
+        maxSal = val2
+
+
+print "Max exercised_stock_options: ", maxESO
+print "Min exercised_stock_options: ", minESO, "\n"
+
+print "Max salary: ", maxSal
+print "Min salary: ", minSal, "\n"
 
 
 
